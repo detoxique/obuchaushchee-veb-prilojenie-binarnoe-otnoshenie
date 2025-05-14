9730ead05a52bed8c1dd8a826158b31d69e1e6f8
detoxique <detoxique@gmail.com>
2025-05-14 18:54:42 +0400

    Реализация localStorage

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,104 @@
 #include <TGUI/Backend/SFML-Graphics.hpp>
 #include <iostream>
 #include <httplib.h>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+class LocalStorage {
+public:
+    bool SetTokens(std::string access_token, std::string refresh_token) {
+        // Открытие файла для записи
+        std::ofstream out_file("localStorage.txt");
+
+        // Проверка, открыт ли файл
+        if (!out_file.is_open()) {
+            std::cerr << "Ошибка открытия файла!" << std::endl;
+            return 1;
+        }
+
+        // Запись строки в файл
+        out_file << "access_token: " << access_token << std::endl;
+        out_file << "refresh_token: " << refresh_token << std::endl;
+
+        // Закрытие файла
+        out_file.close();
+
+        std::cout << "Строка записана в файл." << std::endl;
+        return 0;
+    }
+
+    void Clear() {
+        // Открытие файла для записи
+        std::ofstream out_file("localStorage.txt");
+
+        // Проверка, открыт ли файл
+        if (!out_file.is_open()) {
+            std::cerr << "Ошибка открытия файла!" << std::endl;
+            return;
+        }
+
+        // Запись строки в файл
+        out_file << "access_token: " << std::endl;
+        out_file << "refresh_token: " << std::endl;
+
+        // Закрытие файла
+        out_file.close();
+
+        std::cout << "Строка записана в файл." << std::endl;
+    }
+
+    std::string GetAccessToken() {
+        std::ifstream in("localStorage.txt");
+
+        // Проверка, открыт ли файл
+        if (!in.is_open()) {
+            std::cerr << "Ошибка открытия файла!" << std::endl;
+            return "";
+        }
+
+        std::string line;
+        while (std::getline(in, line)) {
+            if (line.find("access_token: ") == 0) {
+                in.close();
+                return line.substr(14); // Удаляем "access_token: " (14 символов)
+            }
+        }
+    }
+
+    std::string GetRefreshToken() {
+        std::ifstream in("localStorage.txt");
+
+        // Проверка, открыт ли файл
+        if (!in.is_open()) {
+            std::cerr << "Ошибка открытия файла!" << std::endl;
+            return "";
+        }
+
+        std::string line;
+        while (std::getline(in, line)) {
+            if (line.find("refresh_token: ") == 0) {
+                in.close();
+                return line.substr(15); // Удаляем "access_token: " (14 символов)
+            }
+        }
+    }
+};
 
 void login();
+void exit();
+bool verifyToken(const std::string &str_token);
 
 tgui::EditBox::Ptr editBoxLogin;
 tgui::EditBox::Ptr editBoxPassword;
+
+LocalStorage localStorage;
+
+int CurrentPage = 0;
+// 0 - авторизация
+// 1 - список тестов
+// 2 - тест
+
+bool authorized = 0;
 
 int main()
 {
@@ -24,32 +117,69 @@ int main()
         return 1;
     }
 
-    // GUI
-    tgui::Label::Ptr label = tgui::Label::create(u8"Авторизация");
-    label->setPosition({ 560, 200 });
-    label->setTextSize(18);
+    std::string token = localStorage.GetAccessToken();
 
-    editBoxLogin = tgui::EditBox::create();
-    editBoxLogin->setPosition({ 560, 250 });
-    editBoxLogin->setSize({ 160, 20 });
-    editBoxLogin->setTextSize(14);
+    if (verifyToken(token)) {
+        CurrentPage = 1;
+        tgui::Label::Ptr label = tgui::Label::create(u8"Доступные тесты");
+        label->setPosition({ 25, 25 });
+        label->setTextSize(18);
 
-    editBoxPassword = tgui::EditBox::create();
-    editBoxPassword->setPosition({ 560, 290 });
-    editBoxPassword->setSize({ 160, 20 });
-    editBoxPassword->setTextSize(14);
+        auto listBox = tgui::ListBox::create();
+        listBox->setSize(300, 200);
+        listBox->setPosition(25, 60);
 
-    tgui::Button::Ptr button = tgui::Button::create(u8"Войти");
-    button->setPosition({ 610, 330 });
-    button->setSize({ 60, 25 });
-    button->setTextSize(14);
-    button->onClick(login);
+        // Добавление элементов
+        listBox->addItem(u8"Первый элемент");
+        listBox->addItem(u8"Второй элемент");
+        listBox->addItem(u8"Третий элемент");
 
-    gui.add(label);
-    gui.add(button);
-    gui.add(editBoxLogin, "login");
-    gui.add(editBoxPassword, "password");
-    // GUI
+        // Получение выбранного элемента
+        listBox->onItemSelect([](const tgui::String& item, const tgui::String& id) {
+            std::cout << "Выбран: " << item.toStdString()
+                << " (ID: " << id.toStdString() << ")" << std::endl;
+            });
+
+        tgui::Button::Ptr exitButton = tgui::Button::create(u8"Выйти");
+        exitButton->setPosition({ 25, 650 });
+        exitButton->setSize({ 60, 25 });
+        exitButton->setTextSize(14);
+        exitButton->onClick([]() {
+            exit();
+            });
+
+        gui.add(label);
+        gui.add(listBox);
+        gui.add(exitButton);
+    }
+    else {
+        // GUI
+        tgui::Label::Ptr label = tgui::Label::create(u8"Авторизация");
+        label->setPosition({ 560, 200 });
+        label->setTextSize(18);
+
+        editBoxLogin = tgui::EditBox::create();
+        editBoxLogin->setPosition({ 560, 250 });
+        editBoxLogin->setSize({ 160, 20 });
+        editBoxLogin->setTextSize(14);
+
+        editBoxPassword = tgui::EditBox::create();
+        editBoxPassword->setPosition({ 560, 290 });
+        editBoxPassword->setSize({ 160, 20 });
+        editBoxPassword->setTextSize(14);
+
+        tgui::Button::Ptr button = tgui::Button::create(u8"Войти");
+        button->setPosition({ 610, 330 });
+        button->setSize({ 60, 25 });
+        button->setTextSize(14);
+        button->onClick(login);
+
+        gui.add(label);
+        gui.add(button);
+        gui.add(editBoxLogin, "login");
+        gui.add(editBoxPassword, "password");
+        // GUI
+    }
 
     while (window.isOpen())
     {
@@ -60,6 +190,72 @@ int main()
 
             if (event.type == sf::Event::Closed)
                 window.close();
+        }
+
+        if (!authorized && CurrentPage != 0) {
+            gui.removeAllWidgets();
+
+            tgui::Label::Ptr label = tgui::Label::create(u8"Авторизация");
+            label->setPosition({ 560, 200 });
+            label->setTextSize(18);
+
+            editBoxLogin = tgui::EditBox::create();
+            editBoxLogin->setPosition({ 560, 250 });
+            editBoxLogin->setSize({ 160, 20 });
+            editBoxLogin->setTextSize(14);
+
+            editBoxPassword = tgui::EditBox::create();
+            editBoxPassword->setPosition({ 560, 290 });
+            editBoxPassword->setSize({ 160, 20 });
+            editBoxPassword->setTextSize(14);
+
+            tgui::Button::Ptr button = tgui::Button::create(u8"Войти");
+            button->setPosition({ 610, 330 });
+            button->setSize({ 60, 25 });
+            button->setTextSize(14);
+            button->onClick(login);
+
+            gui.add(label);
+            gui.add(button);
+            gui.add(editBoxLogin, "login");
+            gui.add(editBoxPassword, "password");
+            CurrentPage = 0;
+        }
+
+        if (authorized && CurrentPage != 1) {
+            CurrentPage = 1;
+            gui.removeAllWidgets();
+
+            tgui::Label::Ptr label = tgui::Label::create(u8"Доступные тесты");
+            label->setPosition({ 25, 25 });
+            label->setTextSize(18);
+
+            auto listBox = tgui::ListBox::create();
+            listBox->setSize(300, 200);
+            listBox->setPosition(25, 60);
+
+            // Добавление элементов
+            listBox->addItem(u8"Первый элемент");
+            listBox->addItem(u8"Второй элемент");
+            listBox->addItem(u8"Третий элемент");
+
+            // Получение выбранного элемента
+            listBox->onItemSelect([](const tgui::String& item, const tgui::String& id) {
+                std::cout << "Выбран: " << item.toStdString()
+                    << " (ID: " << id.toStdString() << ")" << std::endl;
+                });
+
+            tgui::Button::Ptr exitButton = tgui::Button::create(u8"Выйти");
+            exitButton->setPosition({ 25, 650 });
+            exitButton->setSize({ 60, 25 });
+            exitButton->setTextSize(14);
+            exitButton->onClick([]() {
+                exit();
+                });
+
+            gui.add(label);
+            gui.add(listBox);
+            gui.add(exitButton);
         }
 
         window.clear(sf::Color(255, 255, 255, 255));
@@ -76,6 +272,24 @@ static bool validate() {
     return false;
 }
 
+static bool verifyToken(const std::string& str_token) {
+    if (str_token == "")
+        return 0;
+
+    httplib::Client cli("localhost:8080");
+
+    // POST-запрос с JSON
+    httplib::Headers headers = {
+        {"Authorization", str_token},
+        {"Content-Type", "application/json"}
+    };
+    
+    auto post_res = cli.Post("/api/verify", headers, "", "application/json");
+    if (post_res && post_res->status == 200) {
+        std::cout << "Token verified" << std::endl;
+    }
+}
+
 void login() {
     if (validate()) {
         std::cout << "Validated" << std::endl;
@@ -90,6 +304,58 @@ void login() {
 
     auto post_res = cli.Post("/api/login", headers, body, "application/json");
     if (post_res && post_res->status == 200) {
-        std::cout << "POST Response: " << post_res->body << std::endl;
+        // Парсим и сохраняем токены
+        nlohmann::json j = nlohmann::json::parse(post_res->body);
+
+        std::string access_token = j["access_token"].get<std::string>();
+        std::string refresh_token = j["refresh_token"].get<std::string>();
+        
+        localStorage.SetTokens(access_token, refresh_token);
+        authorized = true;
+    }
+    else {
+        headers = {
+            {"Authorization", localStorage.GetRefreshToken()},
+            {"Content-Type", "application/json"}
+        };
+
+        post_res = cli.Post("/api/refreshtoken", headers, "", "application/json");
+        if (post_res && post_res->status == 200) {
+            nlohmann::json j = nlohmann::json::parse(post_res->body);
+
+            std::string access_token = j["access_token"].get<std::string>();
+
+            localStorage.SetTokens(access_token, localStorage.GetRefreshToken());
+            authorized = true;
+        }
+        else {
+            localStorage.Clear();
+            authorized = false;
+        }
+    }
+}
+
+void exit() {
+    localStorage.Clear();
+    authorized = false;
+}
+
+void getTestsData() {
+    if (!authorized) {
+        return;
+    }
+
+    httplib::Client cli("localhost:8080");
+
+    // POST-запрос с JSON
+    httplib::Headers headers = {
+        {"Content-Type", "application/json"}
+    };
+    std::string body = localStorage.GetAccessToken();
+
+    auto post_res = cli.Post("/api/gettestsdata", headers, body, "application/json");
+
+    if (post_res && post_res->status == 200) {
+
     }
 }
